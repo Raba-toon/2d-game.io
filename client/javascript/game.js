3afd0f67ebe04c7021a5f2b23876b053b8c05535
sam <s.bergeron@theintrovertapp.com>
2025-04-20 18:21:37 -0400

    PUSH\

--- client/javascript/game.js
+++ client/javascript/game.js
@@ -19,3 +19,30 @@ fetch('/client/json/matrice1.json')
     document.body.appendChild(grilleDiv);
   })
   .catch(err => console.error("Erreur chargement grille :", err));
+
+const monId = Math.random().toString(36).substring(2,9);
+const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+const ws = new WebSocket(`${protocol}://${window.location.host}/ws`);
+
+ws.onopen = () => {
+  console.log("WS ouvert, monId =", monId);
+  lastTs = performance.now();
+  requestAnimationFrame(gameLoop);
+};
+
+ws.onmessage = ({ data }) => {
+  const msg = JSON.parse(data);
+  if (msg.type === "positions") {
+    Object.assign(others, msg.positions);
+    delete others[monId];
+  }
+};
+
+ws.onerror = err => console.error("WS erreur :", err);
+
+function gameLoop(ts) {
+  const dt = (ts - lastTs) / 1000;
+  lastTs = ts;
+
+  requestAnimationFrame(gameLoop);
+}
