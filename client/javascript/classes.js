6351e461c793000ccf6fad066915f3e326c0b4e0
Raba-toon <edgarrabatel@gmail.com>
2025-04-20 19:30:07 -0400

    create classe

---
+++ client/javascript/classes.js
@@ -0,0 +1,7 @@
+export class Partie {
+    constructor(players_count, objectives, ennemi_count) {
+      this.players_count = players_count
+      this.objectives = objectives
+      this.ennemi_count = ennemi_count
+    }
+}
